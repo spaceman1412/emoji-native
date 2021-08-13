9c92f9248ece3a6eb6e87fc988435863dfce696f
spaceman1412 <spacemannormal@gmail.com>
2021-08-13 20:02:26 +0700

    Second commit

---
+++ app/src/main/java/com/example/emojiapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.emojiapp
+
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import androidx.core.graphics.drawable.DrawableCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
